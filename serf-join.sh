0dba4ef841021d3b63528ef93139f20cbbdd444c
Eliran Bivas <eliran.bivas@gmail.com>
2014-07-03 16:00:16 +0300

    Update serf-join.sh

--- serf-join.sh
+++ serf-join.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-SERF_ADDR=`env | grep SERF | grep TCP_ADDR | cut -f2 -d"="`
-SERF_PORT=`env | grep SERF | grep "TCP_PORT=" | cut -f2 -d"="`
+SERF_ADDR=`env | grep SERF | grep TCP_ADDR | cut -f2 -d"=" | uniq`
+SERF_PORT=`env | grep SERF | grep "TCP_PORT=" | cut -f2 -d"=" | sort -u | tr "\\n" ","`
 
-echo "Serf at ${SERF_ADDR}:${SERF_PORT}"
+echo "Serf at ${SERF_ADDR}"
 
-SVC=`env | grep -v SERF | grep TCP_ADDR | cut -f1 -d"_" | tr '[:upper:]' '[:lower:]'`
-SVC_ADDR=`env | grep -v SERF | grep TCP_ADDR | cut -f2 -d"="`
-SVC_PORT=`env | grep -v SERF | grep "TCP_PORT=" | cut -f2 -d"="`
+SVC=`env | grep -v SERF | grep TCP_ADDR | cut -f1 -d"_" | tr '[:upper:]' '[:lower:]' | head -1`
+SVC_ADDR=`env | grep -v SERF | grep -i ${SVC} | grep TCP_ADDR | cut -f2 -d"="`
+SVC_PORT=`env | grep -v SERF | grep -i ${SVC} | grep "TCP_PORT=" | cut -f2 -d"="`
 
 echo "Agent role = ${SVC}_${SVC_PORT}"
 echo "Agent address tag = ${SVC}_address=${SVC_ADDR}"
